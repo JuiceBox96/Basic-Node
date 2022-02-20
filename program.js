4e7aee9bceb631bb6e95def2db168415339ac0e9
Terrez <75124951+JuiceBox96@users.noreply.github.com>
2022-02-20 21:29:23 +0000

    Update program.js

--- program.js
+++ program.js
@@ -1,5 +1,5 @@
-let initialMemory = null;
-let word = null;
+let initialMemory = process.memoryUsage().heapUsed;
+let word = process.argv[2];
 
 console.log(`Your word is ${word}`)
 
@@ -12,4 +12,3 @@ for (let i = 0; i < 1000; i++){
 }
 
 console.log(`Starting memory usage: ${initialMemory}. \nCurrent memory usage: ${process.memoryUsage().heapUsed}. \nAfter using the loop to add elements to the array, the process is using ${process.memoryUsage().heapUsed - initialMemory} more bytes of memory.`)
-
